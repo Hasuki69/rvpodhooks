d53606ab4389f8e742b6ed55c292afdbf55792fc
Hasuki <denzfordev@gmail.com>
2024-03-19 15:34:06 +0700

    refactor

--- lib/src/core/themes/theme_slider.dart
+++ lib/src/core/themes/theme_slider.dart
@@ -5,10 +5,18 @@ import '../core.dart';
 class AppSlider {
   static SliderThemeData get light => SliderThemeData(
         // Thumb Shape
-        thumbShape:
-            const RoundSliderThumbShape(elevation: 0, pressedElevation: 0),
-        rangeThumbShape:
-            const RoundRangeSliderThumbShape(elevation: 0, pressedElevation: 0),
+        thumbShape: const RoundSliderThumbShape(
+          elevation: 0,
+          pressedElevation: 0,
+          enabledThumbRadius: .01,
+          disabledThumbRadius: .01,
+        ),
+        rangeThumbShape: const RoundRangeSliderThumbShape(
+          elevation: 0,
+          pressedElevation: 0,
+          enabledThumbRadius: .01,
+          disabledThumbRadius: .01,
+        ),
 
         // Overlap Shape Stroke
         overlappingShapeStrokeColor: Colors.transparent,
@@ -26,10 +34,18 @@ class AppSlider {
 
   static SliderThemeData get dark => SliderThemeData(
         // Thumb Shape
-        thumbShape:
-            const RoundSliderThumbShape(elevation: 0, pressedElevation: 0),
-        rangeThumbShape:
-            const RoundRangeSliderThumbShape(elevation: 0, pressedElevation: 0),
+        thumbShape: const RoundSliderThumbShape(
+          elevation: 0,
+          pressedElevation: 0,
+          enabledThumbRadius: .01,
+          disabledThumbRadius: .01,
+        ),
+        rangeThumbShape: const RoundRangeSliderThumbShape(
+          elevation: 0,
+          pressedElevation: 0,
+          enabledThumbRadius: .01,
+          disabledThumbRadius: .01,
+        ),
 
         // Overlap Shape Stroke
         overlappingShapeStrokeColor: Colors.transparent,
